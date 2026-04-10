b47758593ab4e31493d2f6182e8f800887658c50
kolapep <liawi560@newschool.edu>
2026-04-10 13:36:58 -0400

    Drawing Game Upload: Full Prototype, Unpolished

---
+++ Drawing App/drawingindex.js
@@ -0,0 +1,279 @@
+const canvas = document.getElementById('drawing-board');
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+const uploadInput = document.getElementById('upload');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+let tool = 'brush';
+
+//Undo and Redo
+let undoStack = [];
+let redoStack = [];
+
+//Uploaded Image Manipulation
+let placedImage = null;
+let isDraggingImage = false;
+let isResizingImage = false;
+
+//Click vs Drag detection
+
+canvas.addEventListener('mousedown', (e) => {
+    const mouseX = e.clientX - canvasOffsetX;
+    const mouseY = e.clientY - canvasOffsetY;
+
+    // Save undo
+    undoStack.push(canvas.toDataURL());
+    redoStack = [];
+
+    isPainting = false;
+    isDraggingImage = false;
+    isResizingImage = false;
+
+    if (placedImage) {
+        const handleSize = 10;
+
+        const onHandle =
+            mouseX > placedImage.x + placedImage.width - handleSize &&
+            mouseY > placedImage.y + placedImage.height - handleSize;
+
+        const onImage =
+            mouseX > placedImage.x &&
+            mouseX < placedImage.x + placedImage.width &&
+            mouseY > placedImage.y &&
+            mouseY < placedImage.y + placedImage.height;
+
+        if (onHandle) {
+            isResizingImage = true;
+            return;
+        }
+
+        if (onImage) {
+            isDraggingImage = true;
+            return;
+        }
+    }
+
+    isPainting = true;
+});
+
+//Clear Button
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
+
+//Stroke and Lineweight Buttons
+toolbar.addEventListener('change', e => {
+    if(e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if(e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+    
+});
+
+//Draw and Erase funtions
+const draw = (e) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    if (tool === 'eraser') {
+        ctx.globalCompositeOperation = 'destination-out'; // Drawing
+    } else {
+        ctx.globalCompositeOperation = 'source-over'; // Erase
+    }
+
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY - canvasOffsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener('mouseup', e => {
+    
+    isPainting = false;
+    isDraggingImage = false;
+    isResizingImage = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+//Save button
+
+const saveButton = document.getElementById('save');
+
+saveButton.addEventListener('click', () => {
+    const dataURL = canvas.toDataURL('image/png'); // convert canvas to image
+    const link = document.createElement('a');
+    link.href = dataURL;
+    link.download = `drawing-${Date.now()}.png`;
+    link.click();
+});
+
+//Tool Switch
+
+const eraserButton = document.getElementById('eraser');
+const brushButton = document.getElementById('brush');
+
+eraserButton.addEventListener('click', () => {
+    tool = 'eraser';
+    canvas.style.cursor = 'crosshair';
+});
+
+brushButton.addEventListener('click', () => {
+    tool = 'brush';
+    canvas.style.cursor = 'default';
+});
+
+//Undo and Redo 
+
+const undoButton = document.getElementById('undo');
+const redoButton = document.getElementById('redo');
+
+undoButton.addEventListener('click', () => {
+    if (undoStack.length === 0) return;
+
+    redoStack.push(canvas.toDataURL());
+
+    const imgData = undoStack.pop();
+    const img = new Image();
+    img.src = imgData;
+
+    img.onload = () => {
+        ctx.globalCompositeOperation = 'source-over'; // reset to normal drawing
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+    };
+});
+
+redoButton.addEventListener('click', () => {
+    if (redoStack.length === 0) return;
+
+    undoStack.push(canvas.toDataURL());
+
+    const imgData = redoStack.pop();
+    const img = new Image();
+    img.src = imgData;
+
+    img.onload = () => {
+        ctx.globalCompositeOperation = 'source-over'; // reset to normal drawing
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+    };
+});
+
+//Photo Upload 
+
+uploadInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+        const img = new Image();
+        img.src = event.target.result;
+
+        img.onload = function() {
+            placedImage = {
+            img: img,
+            x: 50,
+            y: 50,
+            width: img.width / 2,
+            height: img.height / 2
+        };
+            render();
+        };
+    };
+    reader.readAsDataURL(file);
+});
+
+//Render Photo
+
+function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw image if it exists
+    if (placedImage) {
+        ctx.drawImage(
+            placedImage.img,
+            placedImage.x,
+            placedImage.y,
+            placedImage.width,
+            placedImage.height
+        );
+
+        drawResizeHandle();
+    }
+}
+
+//Photo Resize
+
+function drawResizeHandle() {
+    const size = 10;
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(
+        placedImage.x + placedImage.width - size,
+        placedImage.y + placedImage.height - size,
+        size,
+        size
+    );
+}
+
+// Photo Movement
+canvas.addEventListener('mousemove', (e) => {
+    const mouseX = e.clientX - canvasOffsetX;
+    const mouseY = e.clientY - canvasOffsetY;
+    canvas.style.cursor = 'default';
+    
+    if (isDraggingImage && placedImage) {
+        placedImage.x = mouseX - placedImage.width / 2;
+        placedImage.y = mouseY - placedImage.height / 2;
+        render();
+        return;
+    }
+
+    if (isResizingImage && placedImage) {
+        const aspect = placedImage.img.width / placedImage.img.height;
+        placedImage.width = mouseX - placedImage.x;
+        placedImage.height = placedImage.width / aspect;
+        render();
+        return;
+    }
+
+    if (placedImage) {
+    const handleSize = 10;
+
+    if (
+        mouseX > placedImage.x + placedImage.width - handleSize &&
+        mouseY > placedImage.y + placedImage.height - handleSize
+    ) {
+        canvas.style.cursor = 'nwse-resize';
+    } else if (
+        mouseX > placedImage.x &&
+        mouseX < placedImage.x + placedImage.width &&
+        mouseY > placedImage.y &&
+        mouseY < placedImage.y + placedImage.height
+    ) {
+        canvas.style.cursor = 'move';
+    }
+    }
+
+    if (isPainting) {draw(e);}
+});
